8cb4fd413f7b332b1c461e6f53d120c3df2cc512
arthur leonardo <leoowarthur@gmail.com>
2026-04-08 12:36:31 -0300

    Add files via upload
    commit do código em Java para o repositório da calculadora de média.

---
+++ CalcMedia.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class CalcMedia {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] nomeAluno = new String[n];
+        int notaAluno = 0;
+    }
+}
+
